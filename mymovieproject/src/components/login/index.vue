64bec7111440bd71f6800f879b22d046baa29ff1
zuer <1974900537@qq.com>
2019-08-12 20:32:20 +0800

    准备登陆功能

--- mymovieproject/src/components/login/index.vue
+++ mymovieproject/src/components/login/index.vue
@@ -1,48 +1,103 @@
 <template>
-  <div class="login-container">
+<div class="login-container">
+  
     <el-form class="login-form" autoComplete="on" :model="loginForm" ref="loginForm" label-position="left">
       <div class="title-container">
-        <h3 class="title">系统登录</h3>
+        
+        <el-tabs v-model="activeName" @tab-click="handleClick" style="height: 200px;" stretch tab-position="top">
+          <el-tab-pane label="系统登录" name="selectLogin">
+
+            <el-form-item prop="username">
+        
+              <span class="svg-container svg-container_login">
+              <svg-icon icon-class="user" />
+              </span>  
+              <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="账号" />
+            </el-form-item>
+
+            <el-form-item prop="password">
+              <span class="svg-container">
+              <svg-icon icon-class="password" />
+              </span> 
+              <el-input name="password" :type="passwordType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on" placeholder="密码" />
+              <span class="show-pwd" @click="showPwd">
+              <svg-icon icon-class="eye" />
+              </span> 
+            </el-form-item>
+            <el-button type="primary" style="width:100%;margin-bottom:40px;float:left;" :loading="loading" @click.native.prevent="handleLogin">登录</el-button>
+
+          </el-tab-pane>
+
+
+
+          <el-tab-pane label="系统注册" name="selectRegister">
+
+
+            <el-form-item prop="usernameRegister">
+        
+              <span class="svg-container svg-container_login">
+              <svg-icon icon-class="user" />
+              </span>  
+              <el-input name="usernameRegister" type="text" v-model="loginForm.usernameRegister"  placeholder="请输入账号" />
+            </el-form-item>
+
+            <el-form-item prop="passwordRegister">
+              <span class="svg-container">
+              <svg-icon icon-class="password" />
+              </span> 
+              <el-input name="passwordRegister" :type="passwordRegisterType" v-model="loginForm.passwordRegister" placeholder="请输入密码" />
+              <span class="show-pwd" @click="showPwdRegister">
+              <svg-icon icon-class="eye" />
+              </span> 
+            </el-form-item>
+
+            <el-form-item prop="passwordRegister2">
+              <span class="svg-container">
+              <svg-icon icon-class="password" />
+              </span> 
+              <el-input name="passwordRegister2" :type="passwordRegisterType" v-model="loginForm.passwordRegister2"  placeholder="再次输入密码" />
+              <span class="show-pwd" @click="showPwdRegister">
+              <svg-icon icon-class="eye" />
+              </span> 
+            </el-form-item>
+
+             
+                <transition name="el-fade-in-linear">
+                <div v-show="show" class="transition-box">密码不一致</div>
+                </transition>
+             
+
+            <el-button type="primary" style="width:100%;margin-bottom:40px;float:right;" :loading="loading" @click.native.prevent="register">注册</el-button>
+
+          </el-tab-pane>
+        </el-tabs>
       </div>
-      <el-form-item prop="username">
-        <span class="svg-container svg-container_login">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="账号" />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input name="password" :type="passwordType" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on" placeholder="密码" />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon icon-class="eye" />
-        </span>
-      </el-form-item>
-
-      <el-button type="primary" style="width:100%;margin-bottom:30px;" :loading="loading" @click.native.prevent="handleLogin">登录</el-button>
-
+      
     </el-form>
   </div>
 </template>
 
 <script>
-import md5 from 'js-md5'
+import md5 from 'js-md5';
 export default {
   name: 'login',
-  data() {
+  data () {
     return {
-      loginForm: {
+     loginForm: {
         username: '',
-        password: ''
+        password: '',
+        usernameRegister:'',
+        passwordRegister:'',
+        passwordRegister2:''
       },
       passwordType: 'password',
+      passwordRegisterType: 'password',
       loading: false,
-      showDialog: false
+      showDialog: false,
+      activeName:'selectLogin',
+      show:false
     }
-  },
-  methods: {
+  },methods:{
     showPwd() {
       if (this.passwordType === 'password') {
         this.passwordType = ''
@@ -50,21 +105,71 @@ export default {
         this.passwordType = 'password'
       }
     },
+    showPwdRegister(){
+      if (this.passwordRegisterType === 'password') {
+        this.passwordRegisterType = ''
+      } else {
+        this.passwordRegisterType = 'password'
+      }  
+    },
+    //登录
     handleLogin() {
       this.loading = true
+      if(this.loginForm.password!=''){
+        this.loginForm.password=md5(this.loginForm.password)
+      }
       this.$store.dispatch('AC_Login', {
         username: this.loginForm.username,
-        password: md5(this.loginForm.password)
+        password: this.loginForm.password
       }).then(res => {
-        this.loading = false
         this.$router.push({ path: '/' })
       }).catch((err) => {
-        this.$store.commit('SHOW_ERROR_TOAST', err.msg || err)        
+        this.$store.commit('SHOW_ERROR_TOAST', err.body.message || err.body)        
       }).finally(() => {
         this.loading = false
       })
      },
+     register(){
+        this.loading = true
+        if(this.loginForm.passwordRegister!=''){
+          this.loginForm.passwordRegister=md5(this.loginForm.passwordRegister)
+        }
+        if(this.loginForm.passwordRegister2!=''){
+          this.loginForm.passwordRegister2=md5(this.loginForm.passwordRegister2)
+        }
+        this.$store.dispatch('AC_REGISTER', {
+          username: this.loginForm.usernameRegister,
+          password: this.loginForm.passwordRegister
+        }).then(res => {
+          this.$notify({title: '注册成功',message: '',type: 'success'});
+          this.$router.push({ path: '/login' })
+        }).catch((err) => {
+          this.$store.commit('SHOW_ERROR_TOAST', err.body.message || err.body)        
+        }).finally(() => {
+          this.loading = false
+        })
+     },
+     handleClick(tab, event) {
+        Object.assign(this.$data, this.$options.data());//置空data数据
+      }
+
   },
+  watch:{
+    'loginForm.passwordRegister2':function(newVal){
+         if(newVal!=''&&newVal!=this.loginForm.passwordRegister){
+           this.show=true;
+         }else{
+           this.show=false;
+         }
+      },
+    'loginForm.passwordRegister':function(newVal){
+        if(this.loginForm.passwordRegister2!=''&&newVal!=this.loginForm.passwordRegister2){
+          this.show=true;
+        }else{
+          this.show=false;
+        }
+    },
+  }
 }
 </script>
 
@@ -87,7 +192,7 @@ $light_gray:#eee;
       color: $light_gray;
       height: 47px;
       &:-webkit-autofill {
-        -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
+        box-shadow: 0 0 0px 1000px $bg inset !important;//-webkit-box-shadow（背景颜色）-webkit表示支持谷歌浏览器，可以不加
         -webkit-text-fill-color: #fff !important;
       }
     }
@@ -171,4 +276,40 @@ $light_gray:#eee;
     bottom: 28px;
   }
 }
+</style>
+<style rel="stylesheet/scss" lang="scss"  scoped>
+/* scoped是有作用域，有时间加上，并依然能达到效果 */
+/* 20190626 采用scss里的deep的做法，可以达到覆盖子标签样式的效果*/
+
+.el-tabs {
+/deep/ .el-tabs__item{
+ color: #acb5cc ;
+ 
+}
+
+
+
+}
+
+.el-tabs {
+ /deep/ .el-tabs__item.is-active {
+    color: #409EFF;
+    font-weight:bold;
+    font-size:150%;
+}
+
+}
+.transition-box {
+    margin-bottom: 10px;
+    width: 100%;
+    
+    border-radius: 4px;
+    background-color: rgb(219, 10, 20);
+    text-align: center;
+    color: #fff;
+    padding: 12px 20px;
+    box-sizing: border-box;
+    margin-right: 20px;
+  }
+ 
 </style>
